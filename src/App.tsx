33efc95103db4ec4d270c7b0f549a6a464a9a792
Walleks Miranda <walleksmiranda@gmail.com>
2024-01-03 22:42:21 -0300

    feat: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>Hello Walleks</h1>;
+  return (
+    <div className="flex  h-screen flex-col items-center justify-center">
+      <h1>Hello Walleks Miranda</h1>
+      <Button>Enviar</Button>
+    </div>
+  )
 }

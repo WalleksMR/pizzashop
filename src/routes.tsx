5a78368cf07a35dcf5c98241298270ab144fe593
Walleks Miranda <walleksmiranda@gmail.com>
2024-01-21 22:49:27 -0300

    feat: add react-helmet-async and page template

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AppAuth } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sing-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
   },
+
   {
-    path: '/sign-in',
-    element: <SignIn />,
+    path: '/',
+    element: <AppAuth />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
   },
 ])

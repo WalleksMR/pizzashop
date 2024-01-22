b06c1c76c7ffa4cb92fbe659b4012311f70aa931
Walleks Miranda <walleksmiranda@gmail.com>
2024-01-21 23:02:51 -0300

    feat: add form

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,5 +1,9 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <div>
@@ -12,7 +16,17 @@ export function SignIn() {
             </h1>
             <p>Acompanhe suas vendas pelo painel do parceiro!</p>
           </div>
-          <div></div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
         </div>
       </div>
     </div>

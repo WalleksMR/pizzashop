a103521e3482b3d3fb03c5b7a811345c52931f35
Walleks Miranda <walleksmiranda@gmail.com>
2024-01-21 23:34:01 -0300

    feat: add toast

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -18,8 +19,18 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
   async function handlSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise<void>((resolve) => setTimeout(resolve, 2000))
+    try {
+      console.log(data)
+      await new Promise<void>((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handlSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
   return (
     <div>

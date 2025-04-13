842a3a129d21596e88614a82fea916b15bc7d247
Walleks Miranda <walleks.rodrigues@teamsoft.com.br>
2025-04-13 12:31:01 -0300

    refactor: integration sign in and sign up

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,91 +1,103 @@
-import { Helmet } from 'react-helmet-async'
-import { useForm } from 'react-hook-form'
-import { Link, useNavigate } from 'react-router-dom'
-import { toast } from 'sonner'
-import { z } from 'zod'
+import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+import { z } from 'zod';
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useMutation } from '@tanstack/react-query';
+import { registerRestaurant } from '@/api/register-restaurant';
 
 const signUpForm = z.object({
   restaurantName: z.string(),
   managerName: z.string(),
   phone: z.string(),
-  email: z.string().email(),
-})
-type SignUpForm = z.infer<typeof signUpForm>
+  email: z.string().email()
+});
+type SignUpForm = z.infer<typeof signUpForm>;
 
 export function SignUp() {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
-  } = useForm<SignUpForm>()
-  const navegate = useNavigate()
+    formState: { isSubmitting }
+  } = useForm<SignUpForm>();
+
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant
+  });
 
   async function handlSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-      await new Promise<void>((resolve) => setTimeout(resolve, 2000))
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone
+      });
+
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
           label: 'Login',
-          onClick: () => navegate('/sign-in'),
-        },
-      })
+          onClick: () => navigate(`/sign-in?email=${data.email}`)
+        }
+      });
     } catch (error) {
-      toast.error('Error ao cadastrar restaurante.')
+      toast.error('Error ao cadastrar restaurante.');
     }
   }
   return (
     <div>
-      <Helmet title="Cadastro" />
-      <div className="p-8">
-        <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-in">Fazer login</Link>
+      <Helmet title='Cadastro' />
+      <div className='p-8'>
+        <Button variant='ghost' asChild className='absolute right-8 top-8'>
+          <Link to='/sign-in'>Fazer login</Link>
         </Button>
-        <div className="flex w-[350px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tighter">
+        <div className='flex w-[350px] flex-col justify-center gap-6'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tighter'>
               Criar conta grátis
             </h1>
             <p>Seja um parceiro e comece suas vendas!</p>
           </div>
 
-          <form onSubmit={handleSubmit(handlSignUp)} className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
+          <form onSubmit={handleSubmit(handlSignUp)} className='space-y-4'>
+            <div className='space-y-2'>
+              <Label htmlFor='restaurantName'>Nome do estabelecimento</Label>
               <Input
-                id="restaurantName"
-                type="text"
+                id='restaurantName'
+                type='text'
                 {...register('restaurantName')}
               />
-              <Label htmlFor="managerName">Seu nome</Label>
+              <Label htmlFor='managerName'>Seu nome</Label>
               <Input
-                id="managerName"
-                type="text"
+                id='managerName'
+                type='text'
                 {...register('managerName')}
               />
 
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" {...register('email')} />
+              <Label htmlFor='email'>Seu e-mail</Label>
+              <Input id='email' type='email' {...register('email')} />
 
-              <Label htmlFor="phone">Seu celular</Label>
-              <Input id="phone" type="tel" {...register('phone')} />
+              <Label htmlFor='phone'>Seu celular</Label>
+              <Input id='phone' type='tel' {...register('phone')} />
             </div>
 
-            <Button disabled={isSubmitting} className="w-full" type="submit">
+            <Button disabled={isSubmitting} className='w-full' type='submit'>
               Finalizar cadastro
             </Button>
 
-            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+            <p className='px-6 text-center text-sm leading-relaxed text-muted-foreground'>
               Ao continuar, você concorda com nossos{' '}
-              <a className="underline underline-offset-4" href="#">
+              <a className='underline underline-offset-4' href='#'>
                 Termos de serviço
               </a>{' '}
               e{' '}
-              <a className="underline underline-offset-4" href="#">
+              <a className='underline underline-offset-4' href='#'>
                 Política de privacidade
               </a>
               .
@@ -94,5 +106,5 @@ export function SignUp() {
         </div>
       </div>
     </div>
-  )
+  );
 }

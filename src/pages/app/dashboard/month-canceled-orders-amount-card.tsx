22deea623ab9624aba7b745b22569d374f647ecb
Walleks Miranda <walleksmiranda@gmail.com>
2025-04-15 08:52:29 -0300

    refactor: integrate get month canceled orders card

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,23 +1,52 @@
-import { DollarSign } from 'lucide-react'
+import { DollarSign } from 'lucide-react';
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-orders-amount';
+import { useQuery } from '@tanstack/react-query';
+import { MetricCardSkeleton } from './matric-card-skeleton';
 
 export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrderAmount } = useQuery({
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
+    queryFn: getMonthCanceledOrdersAmount
+  });
+
   return (
     <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base font-semibold">
+      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+        <CardTitle className='text-base font-semibold'>
           Cancelamentos (mês)
         </CardTitle>
-        <DollarSign className="h-4 w-4 text-muted-foreground" />
+        <DollarSign className='h-4 w-4 text-muted-foreground' />
       </CardHeader>
-      <CardContent className="space-y-1 ">
-        <span className="text-2xl font-bold tracking-tight">32</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">-2%</span> em
-          relação ao mês passado
-        </p>
+      <CardContent className='space-y-1 '>
+        {monthCanceledOrderAmount ? (
+          <>
+            <span className='text-2xl font-bold tracking-tight'>
+              {monthCanceledOrderAmount.amount.toLocaleString('pt-br')}
+            </span>
+            <p className='text-xs text-muted-foreground'>
+              {monthCanceledOrderAmount.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className='text-emerald-500 dark:text-emerald-400'>
+                    +{monthCanceledOrderAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação ao mês passado
+                </>
+              ) : (
+                <>
+                  <span className='text-rose-500 dark:text-rose-400'>
+                    {monthCanceledOrderAmount.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação ao mês passado
+                </>
+              )}
+            </p>
+          </>
+        ) : (
+          <MetricCardSkeleton />
+        )}
       </CardContent>
     </Card>
-  )
+  );
 }
